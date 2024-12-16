d7b6813d33b657c1d5c9cdd6273eabb91dd1978d
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2024-12-16 23:43:03 +0530

    app init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import { MicroappNextFederationPlugin } from '@microapp-io/scripts';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    if (!options.isServer) {
+      config.plugins.push(new MicroappNextFederationPlugin());
+    }
+    if (!options.isServer) {
+      config.plugins.push(new MicroappNextFederationPlugin());
+    }
+    if (!options.isServer) {
+      config.plugins.push(new MicroappNextFederationPlugin());
+    }
+    return config;
+  }
+};
 
 export default nextConfig;
